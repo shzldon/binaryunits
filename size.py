312cc472729e660c87c4477db8c1f94157dd8167
shzldon <26322900+shzldon@users.noreply.github.com>
2020-10-21 22:19:03 +0200

    Change "byte" to "octet"

--- size.py
+++ size.py
@@ -2,165 +2,161 @@ class Size:
 
     """ Constructor """
 
-    def __init__(self, size_in_bytes: int):
+    def __init__(self, size_in_octets: int):
 
-        if not isinstance(size_in_bytes, int):
+        if not isinstance(size_in_octets, int):
             raise TypeError("Size must be an integer")
 
-        if size_in_bytes < 0:
+        if size_in_octets < 0:
             raise ValueError("Size must be positive")
 
-        self._size_in_bytes = size_in_bytes
+        self._size_in_octets = size_in_octets
 
     """ Properties """
 
     @property
-    def B(self) -> int:
-        return self._size_in_bytes
+    def octets(self) -> int:
+        return self._size_in_octets
 
     @property
-    def KiB(self, decimals=None) -> float:
-        """ Size in kibibytes """
-        return float(self._size_in_bytes / 2**10)
+    def o(self) -> int:
+        return self.octets
 
     @property
-    def MiB(self) -> float:
-        """ Size in mebibytes """
-        return float(self._size_in_bytes / 2**20)
+    def Kio(self, decimals=None) -> float:
+        """ Size in kibioctets """
+        return float(self.octets / 2**10)
 
     @property
-    def GiB(self) -> float:
-        """ Size in gibibytes """
-        return float(self._size_in_bytes / 2**30)
+    def Mio(self) -> float:
+        """ Size in mebioctets """
+        return float(self.octets / 2**20)
 
     @property
-    def TiB(self) -> float:
-        """ Size in tebibytes """
-        return float(self._size_in_bytes / 2**40)
+    def Gio(self) -> float:
+        """ Size in gibioctets """
+        return float(self.octets / 2**30)
 
     @property
-    def PiB(self) -> float:
-        """ Size in pebibytes """
-        return float(self._size_in_bytes / 2**50)
+    def Tio(self) -> float:
+        """ Size in tebioctets """
+        return float(self.octets / 2**40)
 
     @property
-    def EiB(self) -> float:
-        """ Size in exbibytes """
-        return float(self._size_in_bytes / 2**60)
+    def Pio(self) -> float:
+        """ Size in pebioctets """
+        return float(self.octets / 2**50)
 
     @property
-    def ZiB(self) -> float:
-        """ Size in zebibytes """
-        return float(self._size_in_bytes / 2**70)
+    def Eio(self) -> float:
+        """ Size in exbioctets """
+        return float(self.octets / 2**60)
 
     @property
-    def YiB(self) -> float:
-        """ Size in yobibytes """
-        return float(self._size_in_bytes / 2**80)
+    def Zio(self) -> float:
+        """ Size in zebioctets """
+        return float(self.octets / 2**70)
 
     @property
-    def kB(self) -> float:
-        """ Size in kilobytes """
-        return float(self._size_in_bytes / 1e3)
+    def Yio(self) -> float:
+        """ Size in yobioctets """
+        return float(self.octets / 2**80)
 
     @property
-    def MB(self) -> float:
-        """ Size in megabytes """
-        return float(self._size_in_bytes / 1e6)
+    def ko(self) -> float:
+        """ Size in kilooctets """
+        return float(self.octets / 1e3)
 
     @property
-    def GB(self) -> float:
-        """ Size in gigabytes """
-        return float(self._size_in_bytes / 1e9)
+    def Mo(self) -> float:
+        """ Size in megaoctets """
+        return float(self.octets / 1e6)
 
     @property
-    def TB(self) -> float:
-        """ Size in terabytes """
-        return float(self._size_in_bytes / 1e12)
+    def Go(self) -> float:
+        """ Size in gigaoctets """
+        return float(self.octets / 1e9)
 
     @property
-    def PB(self) -> float:
-        """ Size in petabytes """
-        return float(self._size_in_bytes / 1e15)
+    def To(self) -> float:
+        """ Size in teraoctets """
+        return float(self.octets / 1e12)
 
     @property
-    def EB(self) -> float:
-        """ Size in exabytes """
-        return float(self._size_in_bytes / 1e18)
+    def Po(self) -> float:
+        """ Size in petaoctets """
+        return float(self.octets / 1e15)
 
     @property
-    def ZB(self) -> float:
-        """ Size in zettabytes """
-        return float(self._size_in_bytes / 1e21)
+    def Eo(self) -> float:
+        """ Size in exaoctets """
+        return float(self.octets / 1e18)
 
     @property
-    def YB(self) -> float:
-        """ Size in yottabytes """
-        return float(self._size_in_bytes / 1e24)
+    def Zo(self) -> float:
+        """ Size in zettaoctets """
+        return float(self.octets / 1e21)
+
+    @property
+    def Yo(self) -> float:
+        """ Size in yottaoctets """
+        return float(self.octets / 1e24)
 
     """ Comparison magic methods """
 
     def __eq__(self, other):
-        return self.B == other.B
+        return self.octets == other.octets
 
     def __ne__(self, other):
-        return self.B != other.B
+        return self.octets != other.octets
 
     def __lt__(self, other):
-        return self.B < other.B
+        return self.octets < other.octets
 
     def __gt__(self, other):
-        return self.B > other.B
+        return self.octets > other.octets
 
     def __le__(self, other):
-        return self.B <= other.B
+        return self.octets <= other.octets
 
     def __ge__(self, other):
-        return self.B >= other.B
+        return self.octets >= other.octets
 
     """ Representations """
 
     def __str__(self):
-        return "%i bytes" % self.B
+        return f"{self.octets} octets"
 
     def __repr__(self):
-        return "%s %i" % (__name__, self.B)
+        return f"{__name__} {self.octets}"
 
     def __hash__(self):
-        return hash(self.B)
+        return hash(self.octets)
 
-    # def __nonzero__(self):
-    #     return bool(self.B)
+    def __nonzero__(self):
+        return self.octets == 0
 
     """ Normal arithmetic operators """
 
     def __add__(self, other):
-        return Size(self.B + other.B)
+        return Size(self.octets + other.octets)
 
     def __sub__(self, other):
-        return Size(self.B - other.B)
+        return Size(self.octets - other.octets)
 
     def __div__(self, other):
-        return self.B / other.B
+        return self.octets / other.octets
 
     def __floordiv__(self, other):
-        return self.B // other.B
+        return self.octets // other.octets
 
     def __mod__(self, other):
-        return Size(self.B % other.B)
+        return Size(self.octets % other.octets)
 
     def __divmod__(self, other):
-        return (self.B // other.B, Size(self.B % other.B))
+        return (self.octets // other.octets, Size(self.octets % other.octets))
 
     def __pow__(self, power):
-        return Size(self.B ** power)
+        return Size(self.octets ** power)
 
-    """ Augmented assigment """
-
-    # TODO : complete this section
-
-    """ Type conversion magic methods """
-
-    # TODO : complete this section
-    
     # TODO : descriptors
